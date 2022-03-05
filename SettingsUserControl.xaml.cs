0845c201fac69b145540bce2b8932cada4b796cf
Min Gu Kang <mingu2001@gmail.com>
2022-03-06 04:27:35 +0900

    📌Code Cleanup📌

--- SettingsUserControl.xaml.cs
+++ SettingsUserControl.xaml.cs
@@ -1,28 +1,18 @@
-﻿using FrontierWidgetFramework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace ClockWidget {
+namespace ClockWidget
+{
     /// <summary>
     /// Interaction logic for SettingsUserControl.xaml
     /// </summary>
-    public partial class SettingsUserControl : UserControl {
+    public partial class SettingsUserControl : UserControl
+    {
 
         private ClockWidgetInstance parent;
 
-        public SettingsUserControl(ClockWidgetInstance parent) {
+        public SettingsUserControl(ClockWidgetInstance parent)
+        {
 
             this.parent = parent;
 
@@ -30,11 +20,13 @@ namespace ClockWidget {
         }
 
 
-        private void checkbox24h_Checked(object sender, RoutedEventArgs e) {
+        private void checkbox24h_Checked(object sender, RoutedEventArgs e)
+        {
             parent.SetClock24h(true);
         }
 
-        private void checkbox24h_Unchecked(object sender, RoutedEventArgs e) {
+        private void checkbox24h_Unchecked(object sender, RoutedEventArgs e)
+        {
             parent.SetClock24h(false);
         }
     }
